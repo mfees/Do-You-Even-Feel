05cc7d63fbfefd2f1244940c1d7cc5b633732845
Carolina Powers <carolinapoloni@gmail.com>
2015-02-19 00:34:46 -0500

    finally figured out how to stream 1 track through our audio html element...going to bed

--- js/main.js
+++ js/main.js
@@ -5,25 +5,41 @@ SC.initialize({
 
 
 $(document).ready(function() {
+//    
+//                 SC.get('http://api.soundcloud.com/playlists/82022730.json?client_id=48726095adc9106006eafbdb701eb2a6', function(playlist) {
+//                    console.log(playlist);   
+//                    for (var i = 0; i < playlist.tracks.length; i++) {
+//                    return playlist.tracks;
+//  }
+//});
     
-                 $.getJSON("http://api.soundcloud.com/playlists/82022730.json?client_id=48726095adc9106006eafbdb701eb2a6", function (dataMad) {
-                   $('.mad').click(function () {
-                           $('iframe').remove();
-                           $('#audio-player').attr('src', dataMad.permalink_url);
-                           $('.music-section').css( "background-color", "#4e4847");       
-                        });
-                    });
-                
-//                $.getJSON("https://soundcloud.com/oembed?maxheight=180&url=https://soundcloud.com/user699424009/sets/what-a-d-k&format=json", function (dataMad) {
+    
+    $.get("https://api.soundcloud.com/tracks/42633860/streams?client_id=48726095adc9106006eafbdb701eb2a6", function(sound){
+        $('#audio-player').attr('src', sound.http_mp3_128_url);
+});
+    
+//             $.getJSON("http://api.soundcloud.com/playlists/82022730.json?client_id=48726095adc9106006eafbdb701eb2a6", function (dataMad) {
 //                   $('.mad').click(function () {
 //                           $('iframe').remove();
-//                           $('.audio-player').append(dataMad.html); 
+//                           $('#audio-player').attr('src', dataMad.permalink_url);
 //                           $('.music-section').css( "background-color", "#4e4847");       
 //                        });
 //                    });
+//                
+                $.getJSON("https://soundcloud.com/oembed?maxheight=180&url=https://soundcloud.com/user699424009/sets/what-a-d-k&format=json", function (dataMad) {
+                   $('.mad').click(function () {
+                          event.preventDefault(); 
+                          $('iframe').remove();
+                           $('#audio-player').remove();
+                           $('.audio-player').append(dataMad.html); 
+                           $('.music-section').css( "background-color", "#4e4847");       
+                        });
+                    });
    
                $.getJSON("https://soundcloud.com/oembed?maxheight=180&url=https://soundcloud.com/user699424009/sets/trollin&format=json", function (dataJoyful) {
                    $('.joyful').click(function () {
+                            event.preventDefault(); 
+                           $('#audio-player').remove();
                            $('iframe').remove();
                            $('.audio-player').append(dataJoyful.html); 
                            $('.music-section').css( "background-color", "#61A598");       
@@ -32,6 +48,8 @@ $(document).ready(function() {
                
                 $.getJSON("https://soundcloud.com/oembed?maxheight=180&url=https://soundcloud.com/user699424009/sets/susanna&format=json", function (dataScared) {
                    $('.scared').click(function () {
+                           event.preventDefault(); 
+                           $('#audio-player').remove();
                            $('iframe').remove();
                            $('.audio-player').append(dataScared.html); 
                            $('.music-section').css( "background-color", "#E2E5EE");       
@@ -40,17 +58,23 @@ $(document).ready(function() {
                 
                 $.getJSON("https://soundcloud.com/oembed?maxheight=180&url=https://soundcloud.com/user699424009/sets/beast-mode&format=json", function (dataPowerful) {
                    $('.powerful').click(function () {
+                           event.preventDefault(); 
+                           $('#audio-player').remove();
                            $('iframe').remove();
                            $('.audio-player').append(dataPowerful.html); 
-                           $('.music-section').css( "background-color", "#66668B");       
+                           $('.music-section').css( "background-color", "#66668B");
+                           $('.music-section').css( "border-bottom", "5px solid #383231");
                         });
                     });
                 
                 $.getJSON("https://soundcloud.com/oembed?maxheight=180&url=https://soundcloud.com/user699424009/sets/thursday-night&format=json", function (dataPeaceful) {
                    $('.peaceful').click(function () {
+                           event.preventDefault(); 
+                           $('#audio-player').remove();
                            $('iframe').remove();
                            $('.audio-player').append(dataPeaceful.html); 
-                           $('.music-section').css( "background-color", "#DDD5A4");       
+                           $('.music-section').css( "background-color", "#DDD5A4");
+                           $('.music-section').css( "border-bottom", "5px solid #569387")
                         });
                     });
                 
@@ -58,6 +82,8 @@ $(document).ready(function() {
                 
                 $.getJSON("https://soundcloud.com/oembed?maxheight=180&url=https://soundcloud.com/user699424009/sets/cuddle-puddle&format=json", function (data){         
                    $('.sad').click(function () {
+                           event.preventDefault(); 
+                           $('#audio-player').remove();
                            $('iframe').remove();
                            $('.audio-player').append(data.html); 
                            $('.music-section').css( "background-color", "#D1CBCD");       
@@ -70,7 +96,7 @@ $(document).ready(function() {
              return playlist.tracks[i].length;
   }
 }); 
-console.log(playlist);
+
 
                 $("<iframe width=\"100%\" height=\"180\" scrolling=\"no\" frameborder=\"no\" src=\"https://w.soundcloud.com/player/?visual=true&url=https%3A%2F%2Fapi.soundcloud.com%2Fplaylists%2F82019062&show_artwork=true&maxheight=180\"></iframe>");
 
